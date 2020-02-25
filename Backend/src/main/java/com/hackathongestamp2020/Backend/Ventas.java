7670ef896de28d45c0f3fb3c26ffd31c4ecc7296
CristianPuga <58120121+CristianPuga@users.noreply.github.com>
2020-02-25 20:21:59 +0100

    Add files via upload

--- Backend/src/main/java/com/hackathongestamp2020/Backend/Ventas.java
+++ Backend/src/main/java/com/hackathongestamp2020/Backend/Ventas.java
@@ -13,7 +13,7 @@ public class Ventas {
     public String country;
     public String item_type;
     public String sales_channel;
-    public Character order_priority;
+    public String order_priority;
     public Date order_date;
     public Integer order_id;
     public String ship_date;
@@ -23,4 +23,36 @@ public class Ventas {
     public String total_revenue;
     public String total_cost;
     public String total_profit;
+
+
+    public Ventas(
+            final String region,
+            final String country,
+            final String item_type,
+            final String sales_channel,
+            final String order_priority,
+            final Date order_date,
+            final Integer order_id,
+            final String ship_date,
+            final Integer units_sold,
+            final String unit_price,
+            final String unit_cost,
+            final String total_revenue,
+            final String total_cost,
+            final String total_profit ){
+        this.region = region;
+        this.country = country;
+        this.item_type = item_type;
+        this.sales_channel = sales_channel;
+        this.order_priority = order_priority;
+        this.order_date = order_date;
+        this.order_id = order_id;
+        this.ship_date = ship_date;
+        this.units_sold = units_sold;
+        this.unit_price = unit_price;
+        this.unit_cost = unit_cost;
+        this.total_revenue = total_revenue;
+        this.total_cost = total_cost;
+        this.total_profit = total_profit;
+    }
 }
