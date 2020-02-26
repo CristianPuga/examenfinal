40bc52054d646070afb34ec913f80ed640cac693
CristianPuga <58120121+CristianPuga@users.noreply.github.com>
2020-02-26 20:55:36 +0100

    Add files via upload

--- Backend/src/main/java/com/hackathongestamp2020/Backend/BackendApplication.java
+++ Backend/src/main/java/com/hackathongestamp2020/Backend/BackendApplication.java
@@ -2,8 +2,11 @@ package com.hackathongestamp2020.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class BackendApplication {
 
 	public static void main(String[] args) {
